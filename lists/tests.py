50b170ef334b9d67588dfaf6d5c5210119aa87da
acecodes <real.deal.ace@gmail.com>
2014-04-15 15:36:04 -0700

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
