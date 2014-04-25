58994e9080316e14304e0f9d2f1863a54b826d19
Ace <real.deal.ace@gmail.com>
2014-04-25 16:03:43 -0700

    Added a new test case, still under construction

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -33,6 +33,9 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# User hits enter, the page updates and the page lists the user's item
 		inputbox.send_keys(Keys.ENTER)
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
@@ -49,6 +52,36 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
+		# The page updates again, showing both items on the user's list
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+		# A new user enters the site and begins to use it
+
+		## Use a new browser session to ensure that no information from the previous user is left behind ##
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		# The new user opens the page and sees there is a new, blank list available
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		# The new user creates a new list by entering a new item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		# A new, unique URL is generated for the new user
+		francis_list_url = self.browser.current_url
+		selt.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		# Once again, there is no evidence of the previous user
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
 
 		# Placeholder for future test development
 		self.fail('Finish the test!')
